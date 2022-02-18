4521e694a595b4663be02e31661b8233cb40ef6c
Abbascloud <mamedovan91@yandex.ru>
2022-02-18 18:24:45 +0300

    hw4

--- hw3/index.js
+++ hw3/index.js
@@ -3,20 +3,24 @@ const fs = require("fs");
 const ACCESS_LOG = "./access.log";
 
 const FILTERED_FIRST_LOG = "./34.48.240.111_requests.log";
+
 const FILTERED_SECOND_LOG = "./89.123.1.41_requests.log";
 
 const readStream = fs.createReadStream(ACCESS_LOG, { encoding: "utf-8" });
 readStream.on("data", (chunk) => {
   let logs = chunk.match(/^89\.123\.1\.41.+|34\.48\.240\.111.+$/gm);
+
   let FILTERED_LOG = "";
 
   logs.forEach((filteredString) => {
     if (/^34\.48\.240\.111.+$/.test(filteredString)) {
       FILTERED_LOG = FILTERED_FIRST_LOG;
     }
+
     if (/^89\.123\.1\.41.+$/.test(filteredString)) {
       FILTERED_LOG = FILTERED_SECOND_LOG;
     }
+
     fs.writeFile(
       FILTERED_LOG,
       filteredString + "\n",
