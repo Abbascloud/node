4dd96e4ee381b1997102d5b756f10bbcdffb4707
Abbascloud <mamedovan91@yandex.ru>
2022-02-22 00:11:29 +0300

    hw-4remake

--- hw4/cli.js
+++ hw4/cli.js
@@ -5,9 +5,13 @@ const path = require("path");
 const fs = require("fs");
 const inquirer = require("inquirer");
 const { choices } = require("yargs");
+const { error } = require("console");
+const { resolve } = require("path/posix");
+const { rejects } = require("assert");
 
 let currentDir = process.cwd();
-const options = yargs
+console.log(currentDir);
+const DirOptions = yargs
   .positional("d", {
     describe: "path to dir",
     default: process.cwd(),
@@ -16,8 +20,6 @@ const options = yargs
     describe: "Pattern",
     default: "",
   }).argv;
-
-console.log(options);
 
 class DirItem {
   constructor(path, fileName) {
@@ -29,38 +31,64 @@ class DirItem {
   }
 }
 
-const start = async () => {
-  const list = await fs.readdir(currentDir);
-  const items = list.map((dirOrFile) => {
-    new DirItem(path.join(currentDir, dirOrFile), dirOrFile);
+class Dir {
+  constructor(path = currentDir, options = DirOptions) {
+    this.path = path;
+    this.items = [];
+    this.options = options;
+    this._init();
+  }
+  async _init() {
+    await this._start();
+  }
 
-    const item = async () =>
-      await inquirer
-        .prompt([
-          {
-            name: "fileName",
-            type: "list",
-            message: `choose: ${currentDir}`,
-            choices: items.map((item) => ({
-              name: item.fileName,
-              value: item,
-            })),
-          },
-        ])
-        .then((answer) => answer.fileName);
-    if (item.isDir) {
-      currentDir = item.path;
-      return start();
-    } else {
-      const data = async () => await fs.readFile(item.path, "utf-8");
-
-      if (options.p == null) console.log(data);
-      else {
-        const regExp = new RegExp(options.p, "igm");
-        console.log(data.match(regExp));
+  async _start() {
+    const list = await fs.readdir(this.path, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        data.forEach((fileName) => {
+          let newItem = new DirItem(path.join(this.path, fileName), fileName);
+          this.items.push(newItem);
+        });
       }
-    }
-  });
-};
 
-start();
+      (async () => {
+        const item = await inquirer
+          .prompt([
+            {
+              name: "filename",
+              type: "list",
+              message: `Choose ${this.path}`,
+              choices: this.items.map((item) => ({
+                name: item.fileName,
+                value: item,
+              })),
+            },
+          ])
+          .then((answer) => answer.filename);
+
+        if (item.isDir) {
+          this.path = item.path;
+          return await this._start();
+        } else {
+          await fs.readFile(item.path, { encoding: "utf-8" }, (err, data) => {
+            if (err) {
+              console.log(err);
+            } else {
+              if (!this.options.p) {
+                console.log(data);
+              } else {
+                const regExp = new RegExp(this.options.p, "igm");
+                console.log(data.match(regExp));
+              }
+            }
+          });
+        }
+      })();
+    });
+  }
+}
+
+let dir = new Dir();
+console.log(dir.options);
